e92f3583c10c3ebe9019471682ae9faf8b8cf5e4
Szabolcs Dombi <cprogrammer1994@gmail.com>
2021-03-05 08:50:28 +0200

    add stub file

---
+++ glnext_compiler-stubs/__init__.pyi
@@ -0,0 +1,11 @@
+def glsl(source: str) -> bytes:
+    '''
+        Compile GLSL into SPV
+
+            #version 450
+            #pragma shader_stage(vertex)
+
+            void main() {
+            }
+    '''
+    pass
